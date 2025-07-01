2d48838d772e41253ff710f6aef4dc98dee2a3dc
marioonofrio <marofrio7@gmail.com>
2025-07-01 18:40:33 -0500

    includes command execution as well as pipeline and redirection

--- main.c
+++ main.c
@@ -26,6 +26,137 @@ void signal_handling(int sig)
     }
 }
 
+void handle_redirection(char *args[], int *input_fd, int *output_fd)
+{
+    for (int i = 0; args[i] != NULL; i++)
+    {
+        if (strcmp(args[i], "<") == 0)
+        {
+            if (args[i + 1] == NULL)
+            {
+                fprintf(stderr, "Error: No input file specified for redirection\n");
+                return;
+            }
+            *input_fd = open(args[i + 1], O_RDONLY);
+            if (*input_fd < 0)
+            {
+                perror("Error opening input file");
+                return;
+            }
+            args[i] = NULL;
+            break;
+        }
+        else if (strcmp(args[i], ">") == 0)
+        {
+            if (args[i + 1] == NULL)
+            {
+                fprintf(stderr, "Error: No output file specified for redirection\n");
+                return;
+            }
+            *output_fd = open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (*output_fd < 0)
+            {
+                perror("Error opening output file");
+                return;
+            }
+            args[i] = NULL;
+            break;
+        }
+    }
+}
+
+void handle_pipeline(char *commands[], int num)
+{
+    int pipe_fds[2];
+    int input_fd = STDIN_FILENO;
+    pid_t pid;
+
+    for (int i = 0; i < num; i++)
+    {
+        if (i < num - 1 && pipe(pipe_fds) < 0)
+        {
+            perror("Error creating pipe");
+            exit(EXIT_FAILURE);
+        }
+
+        pid = fork();
+        if (pid == 0)
+        {
+            if (input_fd != STDIN_FILENO)
+            {
+                dup2(input_fd, STDIN_FILENO);
+                close(input_fd);
+            }
+
+            if (i < num - 1)
+            {
+                dup2(pipe_fds[1], STDOUT_FILENO);
+                close(pipe_fds[0]);
+                close(pipe_fds[1]);
+            }
+            char *args[MAX_ARGS];
+            int num_args;
+            parse_command(commands[i], args, &num_args);
+
+            if (execvp(args[0], args) == -1)
+            {
+                perror("Error executing command in pipeline");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else if (pid > 0)
+        {
+            waitpid(pid, NULL, 0);
+            if (input_fd != STDIN_FILENO)
+            {
+                close(input_fd);
+            }
+            if (i < num - 1)
+            {
+                close(pipe_fds[1]);
+                input_fd = pipe_fds[0];
+            }
+        }
+        else
+        {
+            perror("Error forking process");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void execute_command(char *args[])
+{
+    if (args[0] == NULL)
+    {
+        return;
+    }
+
+    int num_pipes = 0;
+    for (int i = 0; args[i] != NULL; i++)
+    {
+        if (strcmp(args[i], "|") == 0)
+        {
+            num_pipes++;
+        }
+    }
+
+    if (num_pipes > 0)
+    {
+        char *pipeline_commands[MAX_ARGS];
+        int num_pipeline_commands = 0;
+        char *command = strtok(args[0], "|");
+        while (command != NULL)
+        {
+            pipeline_commands[num_pipeline_commands++] = command;
+            command = strtok(NULL, "|");
+        }
+
+        handle_pipeline(pipeline_commands, num_pipeline_commands);
+        return;
+    }
+}
+
 void interactive_mode()
 {
     char input[MAX_INPUT_SIZE];
